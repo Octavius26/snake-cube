da58e195a7daf2576e4002d39d23ca716ddaf39d
Octavius26 <al.compte@ld26.com>
2023-02-02 01:43:24 +0100

    Ajout de plot3D

--- solver_back.py
+++ solver_back.py
@@ -64,32 +64,34 @@ def dir_possibilities( cube:T_cube,
 
 
 def add_to_cube(cube:T_cube,line,dir) -> tuple[bool,T_cube]:
-    cube = cube.copy()
+    cube__ = cube.copy()
 
 
-    M = cube.M
-    x,y,z = cube.end_pointer
+    M = cube__.M
+    x,y,z = cube__.end_pointer
     match dir :
         case 'x':
             for i in range(1,line): M[x+i][y][z] += 1
-            cube.end_pointer = (x+(line-1),y,z)
+            cube__.end_pointer = (x+(line-1),y,z)
         case '-x':
             for i in range(1,line): M[x-i][y][z] += 1
-            cube.end_pointer = (x-(line-1),y,z)
+            cube__.end_pointer = (x-(line-1),y,z)
         case 'y':
             for i in range(1,line): M[x][y+i][z] += 1
-            cube.end_pointer = (x,y+(line-1),z)
+            cube__.end_pointer = (x,y+(line-1),z)
         case '-y':
             for i in range(1,line): M[x][y-i][z] += 1
-            cube.end_pointer = (x,y-(line-1),z)
+            cube__.end_pointer = (x,y-(line-1),z)
         case 'z':
             for i in range(1,line): M[x][y][z+i] += 1
-            cube.end_pointer = (x,y,z+(line-1))
+            cube__.end_pointer = (x,y,z+(line-1))
         case '-z':
             for i in range(1,line): M[x][y][z-i] += 1
-            cube.end_pointer = (x,y,z-(line-1))
-
-    return (False, None) if 2 in M else (True, cube)
+            cube__.end_pointer = (x,y,z-(line-1))
+        case _ :
+            raise ValueError()
+    cube__.list_dir.append(dir)
+    return (False, None) if 2 in M else (True, cube__)
 
 
 
@@ -124,17 +126,20 @@ def rec_solver( cube : T_cube,
         minimal_len = N
     # else : print("=>",N)
 
+    cube_sol = None
     for dir in l_new_dir:
         test_ok,new_cube = add_to_cube(cube,line,dir)
         if test_ok: 
-            new_cube.list_dir.append(dir)
+            # new_cube.list_dir.append(dir)
             sol_found,cube_sol,minimal_len_2 = rec_solver(new_cube, snake,minimal_len)
             if minimal_len_2 < minimal_len :
                 minimal_len = minimal_len_2
             if sol_found :
                 return True,cube_sol,0
     if l_new_dir == [] : print(".",end='')
-    return False,None,minimal_len
+    if cube_sol is None :
+        return False,cube,minimal_len
+    return False,cube_sol,minimal_len_2
 
 
 def solve(snake:list[int],size,start_point = (0,0,0))->T_cube:
@@ -146,3 +151,77 @@ def solve(snake:list[int],size,start_point = (0,0,0))->T_cube:
     cube_init.M[x,y,z] = 1
     sol_found,cube_sol,minimum_len = rec_solver(cube_init,snake,len(snake))
     return cube_sol
+
+
+
+
+import plotly.graph_objs as go
+
+def plot_cube(cube:T_cube):
+
+	x,y,z = [],[],[]
+
+	N = cube.size
+	for i in range(N):
+		for j in range(N):
+			for k in range(N):
+				x.append(i)
+				y.append(j)
+				z.append(k)
+
+	data = go.Scatter3d(x=x,y=y,z=z,
+						mode='markers',
+						marker=dict(size=12,opacity=0.8,color="#555"))
+	
+	x,y,z = [],[],[]
+
+	N = cube.size
+	for i in range(N):
+		for j in range(N):
+			for k in range(N):
+				if cube.M[i,j,k] == 1:
+					x.append(i)
+					y.append(j)
+					z.append(k)
+
+	data2 = go.Scatter3d(x=x,y=y,z=z,
+						mode='markers',
+						marker=dict(size=12,opacity=0.5,color="#0a0"))
+	
+	x,y,z = [],[],[]
+
+	N = cube.size
+	for i in range(N):
+		for j in range(N):
+			for k in range(N):
+				if cube.M[i,j,k] == 2 :
+					x.append(i)
+					y.append(j)
+					z.append(k)
+
+	data3 = go.Scatter3d(x=x,y=y,z=z,
+						mode='markers',
+						marker=dict(size=12,opacity=0.5,color="#f00"))
+	
+
+		
+	x,y,z = [],[],[]
+
+	N = cube.size
+	for i in range(N):
+		for j in range(N):
+			for k in range(N):
+				if cube.M[i,j,k] < 0 or cube.M[i,j,k] > 2:
+					x.append(i)
+					y.append(j)
+					z.append(k)
+
+	data4 = go.Scatter3d(x=x,y=y,z=z,
+						mode='markers',
+						marker=dict(size=12,opacity=1,color="#000"))
+	
+	
+	
+	layout = go.Layout(margin=dict(l=0,r=0,b=0,t=0))
+	fig = go.Figure(data=[data,data2,data3,data4], layout=layout)
+	fig.show()
